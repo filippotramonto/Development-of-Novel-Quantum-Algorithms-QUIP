1494cdb62fc01de5173955e412915c1b835d0b35
Filippo Tramonto <11334590+filippotramonto@users.noreply.github.com>
2024-08-09 21:13:22 +0200

    uploaded folders with files

---
+++ Project Step 1/ancilla.py
@@ -0,0 +1,86 @@
+"""
+This module contains classes for managing ancilla qubits of the
+circuits defined in the other files. Because the protocol this
+code is designed for can discard "garbage" ancilla bits through
+a measurement scheme, the ancilla managers here include the ability
+to both discard ancillas and also re-use ancillas which have
+already been discarded.
+
+(c) Gregory D. Kahanamoku-Meyer 2021
+"""
+
+import cirq
+from tof_sim import int_to_state
+
+
+class Ancilla(cirq.NamedQubit):
+
+    def __init__(self, idx):
+        cirq.NamedQubit.__init__(self, f'anc{idx}')
+        self._isgarbage = False
+
+    @property
+    def isgarbage(self):
+        return self._isgarbage
+
+    def _discard(self):
+        self._isgarbage = True
+
+
+class AncillaManager:
+
+    def __init__(self):
+        self._qubits = []
+        self._n_active = 0
+        self._max_ancillas = 0
+        self._n_meas = 0
+
+    def new(self):
+        q = Ancilla(len(self._qubits))
+        self._qubits.append(q)
+        self.n_active += 1
+        return q
+
+    def new_register(self, n):
+        rtn = [Ancilla(len(self._qubits)+i) for i in range(n)]
+        self._qubits += rtn
+        self.n_active += n
+        return rtn
+
+    def discard(self, qubits):
+        if isinstance(qubits, Ancilla):
+            qubits = [qubits]
+
+        for qubit in qubits:
+            qubit._discard()
+            self._n_meas += 1
+
+        self.n_active -= len(qubits)
+
+    def all(self):
+        return self._qubits
+
+    def init_state(self):
+        return int_to_state(0, self._qubits)
+
+    def all_discarded(self):
+        return self.n_active == 0
+
+    def max_ancilla_usage(self):
+        return self._max_ancillas
+
+    def count_measurements(self):
+        return self._n_meas
+
+    @property
+    def n_active(self):
+        return self._n_active
+
+    @n_active.setter
+    def n_active(self, val):
+        self._n_active = val
+        if self.n_active > self._max_ancillas:
+            self._max_ancillas = self.n_active
+
+    def __len__(self):
+        return len(self._qubits)
---
+++ Project Step 1/tof_sim.py
@@ -0,0 +1,84 @@
+"""
+This file contains a class for simulating quantum circuits
+that contain only X, CNOT, and Toffoli gates (as well as
+possibly Y and Z "error" gates), when applied to a product
+state initial state. The simulation is polynomial-time in
+the number of qubits (linear in the number of gates), and
+thus can be applied to very large circuits.
+
+(c) Gregory D. Kahanamoku-Meyer 2021
+"""
+
+import cirq
+
+class ToffoliSimulator:
+
+    allowed_gates = {
+        cirq.TOFFOLI,
+        cirq.CNOT,
+        cirq.X,
+    }
+
+    phase_error_gates = {
+        cirq.Y,
+        cirq.Z
+    }
+
+    def __init__(self, c, qubits=None):
+        self.circuit = c
+
+        if qubits is None:
+            qubits = list(c.all_qubits())
+        self.qubits = qubits
+
+        self.phase = 1
+
+    def simulate(self, state):
+        """
+        Simulate the circuit, returning the result as an integer
+
+        Arguments
+        ---------
+
+        state : dict
+            A dictionary mapping qubit objects to their initial state. Will
+            be modified to contain the result.
+        """
+        phase = 1
+        for moment in self.circuit:
+            for op in moment:
+                if op.gate in self.allowed_gates:
+                    if all(state[q.name] for q in op.qubits[:-1]):
+                        state[op.qubits[-1].name] ^= 1
+
+                elif op.gate in self.phase_error_gates:
+                    if state[op.qubits[0].name] == 1:
+                        phase *= -1
+
+                    # Y also flips the bit
+                    if op.gate is cirq.Y:
+                        state[op.qubits[-1].name] ^= 1
+
+                else:
+                    raise ValueError(f"unsupported gate type '{op.gate}'")
+
+        # don't need to return state; it is modified in-place
+        return phase
+
+
+def int_to_state(x, reg):
+    if x.bit_length() > len(reg):
+        raise ValueError("integer too large for register")
+
+    rtn = {
+        q.name : (x>>i)&1 for i,q in enumerate(reg)
+    }
+
+    return rtn
+
+def state_to_int(state, reg):
+    rtn = 0
+    for q in reg[::-1]:
+        rtn <<= 1
+        rtn |= state[q.name]
+    return rtn
